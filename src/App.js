f2f1b8aca3905a1fe4c21885d9a9330402fdbe4c
Talibiq123 <talibsaeed90@gmail.com>
2024-01-01 17:29:50 +0530

    Accordian

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Accordian from './001_Accordian/Components/Accordian';
+
+function App() {
+  return (
+    <div >
+    <Accordian />
+    </div>
+  );
+}
+
+export default App;
