3f2bc2571b0ac482ea5ac03313d14bc78f2c47cc
Talibiq123 <talibsaeed90@gmail.com>
2024-01-02 21:09:09 +0530

    single accordian

--- src/Components/001_Accordian/Accordian.js
+++ src/Components/001_Accordian/Accordian.js
@@ -1,25 +1,31 @@
 import React, { useState } from 'react';
 import data from './data';
+import './style.css';
+
 
 function Accordion() {
     const [selected, setSelected] = useState(null);
 
     
     function handleSingleClick(getCurrentId) {
-        setSelected(getCurrentId);
+        console.log(getCurrentId);
+        setSelected(getCurrentId === selected? null: getCurrentId);
     }
 
     return (
-        <div className='Wrapper'>
+        <div className='wrapper'>
         <div className='accordian'>
             {
                 data && data.length > 0 ? (
                     data.map((dataItem) => (
                         <div className='item'>
-                            <div onClick={handleSingleClick} className='title'>
-                            <h3>{dataItem.question}</h3>
-                            <span>+</span>
+                            <div onClick={() => handleSingleClick(dataItem.id)} className='title'>
+                                <h3>{dataItem.question}</h3>
+                                <span className='mid'>+</span>
                             </div>
+                            {
+                                selected === dataItem.id ? <div>{dataItem.answer}</div> : <div>{null}</div>
+                            }
                         </div>
                     )
                 )): <div>No Data is found</div>
