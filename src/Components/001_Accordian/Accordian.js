a6667a334661d6af2e429cfb9de613302f3d0a03
Talibiq123 <talibsaeed90@gmail.com>
2024-01-03 17:53:55 +0530

    accordian

--- src/Components/001_Accordian/Accordian.js
+++ src/Components/001_Accordian/Accordian.js
@@ -5,6 +5,8 @@ import './style.css';
 
 function Accordion() {
     const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
 
     
     function handleSingleClick(getCurrentId) {
@@ -12,14 +14,25 @@ function Accordion() {
         setSelected(getCurrentId === selected? null: getCurrentId);
     }
 
+    function handleMultiCLick(getCurrentId) {
+        let copyMultiple = [...multiple];
+        const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+
+        if(findIndexOfCurrentId == -1) copyMultiple.push(getCurrentId);
+        else copyMultiple.splice(findIndexOfCurrentId, 1);
+    }
+
     return (
         <div className='wrapper'>
+            <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
         <div className='accordian'>
             {
                 data && data.length > 0 ? (
                     data.map((dataItem) => (
                         <div className='item'>
-                            <div onClick={() => handleSingleClick(dataItem.id)} className='title'>
+                            <div onClick={enableMultiSelection?
+                            () => handleMultiClick(dataItem.id)
+                            : () => handleSingleClick(dataItem.id)} className='title'>
                                 <h3>{dataItem.question}</h3>
                                 <span className='mid'>+</span>
                             </div>
