41a72fbd477c2fab0575514033396b04e208e622
Talibiq123 <talibsaeed90@gmail.com>
2024-01-05 19:49:19 +0530

    Image Slider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import './App.css';
-import StarRating from './Components/003_Star_Rating/StarRating';
+import "./App.css";
+import ImageSlider from "./Components/004_Image_Slider/ImageSlider";
+// import StarRating from './Components/003_Star_Rating/StarRating';
 // import RandomColor from './Components/002_Random_Color_Generator/RandomColor';
 // import Accordion from './Components/001_Accordian/Accordian';
 
 function App() {
   return (
-    <div >
+    <div>
       {/* <Accordion /> */}
 
       {/* Random Color Generator */}
       {/* <RandomColor /> */}
 
       {/* star rating */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image Slider */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
