ec654d1cc03ab7a53a75c6b250b77c8697a3c3e2
Talibiq123 <talibsaeed90@gmail.com>
2024-01-06 19:54:45 +0530

    Load More

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import ImageSlider from "./Components/004_Image_Slider/ImageSlider";
+// import ImageSlider from "./Components/004_Image_Slider/ImageSlider";
+import LoadMore from "./Components/005_Load_More_Data/LoadMore";
 // import StarRating from './Components/003_Star_Rating/StarRating';
 // import RandomColor from './Components/002_Random_Color_Generator/RandomColor';
 // import Accordion from './Components/001_Accordian/Accordian';
@@ -16,11 +17,15 @@ function App() {
       {/* <StarRating noOfStars={10}/> */}
 
       {/* Image Slider */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+
+
+      {/* Load More Data */}
+      <LoadMore />
     </div>
   );
 }
